7b2f89349c79a89f2e975c8f31a9c378fa45e108
Kazuya MIURA <miup.0627@gmail.com>
2015-01-26 14:36:40 +0900

    Initial Commit

---
+++ CalculatorKeyboardApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorKeyboardApp
+//
+//  Created by kazuya on 1/26/15.
+//  Copyright (c) 2015 COINS Project AID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
